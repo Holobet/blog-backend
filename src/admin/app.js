790b7df6a4ed8c78df1c796f9af6ad772d070dfa
Shahzeb Khan <shahzeb8285@gmail.com>
2023-07-08 07:32:05 +0300

    fixed

---
+++ src/admin/app.js
@@ -0,0 +1,18 @@
+// @ts-ignore
+import favicon from './extensions/favicon.png';
+
+export default {
+  config: {
+    head: {
+      favicon: favicon,
+    },
+    tutorials: false,
+    translations: {
+      en: {
+        "Auth.form.welcome.title":"Welcome",
+        "Auth.form.welcome.subtitle": "Log in to your HBT Blog account",
+        "app.components.LeftMenu.navbrand.title":"Holobet Blog"
+      },
+    },
+  }
+}
