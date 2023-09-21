74b8faad75ddf84b3a2d777fb778fd7ef97f2e96
yunawinaya <gedeyuna@gmail.com>
2023-09-21 17:10:44 +0800

    quiz user

---
+++ src/api/quiz-user/controllers/quiz-user.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * quiz-user controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::quiz-user.quiz-user');
